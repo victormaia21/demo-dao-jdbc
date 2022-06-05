e888550dedf331803259e79446fb95186dceed1a
victor maia <jv1555550@gmail.com>
2022-06-05 16:34:42 -0500

    SellerDaoJDBC and DaoFactory

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,11 +1,14 @@
 package Application;
 
+import model.dao.Daofactory;
+import model.dao.Sellerdao;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 
-		
+		Sellerdao sellerdao = Daofactory.createselleSellerdao();
 		
 	}
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -2,37 +2,33 @@ package model.dao.impl;
 
 import java.util.List;
 
+import model.dao.Sellerdao;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements Sellerdao{
 
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void Deletebyid(Integer id) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public Seller findbyid(Integer id) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public List<Seller> findall() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
